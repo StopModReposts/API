d7eaf38279d4cdd3b765a1edfb1147966b1f6d3a
berrysauce <contact@berrysauce.me>
2021-06-05 10:54:19 +0200

    Added stat counter and txt response

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import RedirectResponse, StreamingResponse, PlainTextResponse
 import air_telemetry as telemetry
 from dotenv import load_dotenv
@@ -7,12 +7,13 @@ import os
 from deta import Deta
 from typing import Optional
 import yaml
+from datetime import date, datetime
 
 
 """
 Responses:
 - yaml [DONE]
-- txt
+- txt [DONE]
 - json [DONE]
 - xml
 - hosts.txt
@@ -30,15 +31,31 @@ app = FastAPI()
 logger = telemetry.Endpoint("https://telemetry.brry.cc", "smr-api", TELEMETRY_TOKEN)
 deta = Deta(DETA_TOKEN)
 drive = deta.Drive("formats")
-db = deta.Base("formats")
+stats = deta.Base("smr-stats")
 
+
+def statcounter():
+    try:
+        month = str(datetime.now().month)
+        request = next(stats.fetch({"month": month}))[0]
+        stats.put({
+            "month": month,
+            "total": int(request["total"]) + 1
+        }, request["key"])
+    except:
+        month = str(datetime.now().month)
+        stats.insert({
+            "month": month,
+            "total": 1
+        })
 
 @app.get("/")
 def root():
     return RedirectResponse("https://stopmodreposts.org")
 
 @app.get("/sites.yaml")
-def get_yaml(game: Optional[str] = None):
+def get_yaml(background_tasks: BackgroundTasks, game: Optional[str] = None):
+    background_tasks.add_task(statcounter)
     if game is None:
         try:
             res = drive.get("minecraft.yaml")
@@ -53,7 +70,8 @@ def get_yaml(game: Optional[str] = None):
             return {"msg": "Error - No file available"}
         
 @app.get("/sites.json")
-def get_json(game: Optional[str] = None):
+def get_json(background_tasks: BackgroundTasks, game: Optional[str] = None):
+    background_tasks.add_task(statcounter)
     if game is None:
         res = drive.get("minecraft.yaml")
         return yaml.load(res.read(), Loader=yaml.FullLoader)
@@ -61,6 +79,23 @@ def get_json(game: Optional[str] = None):
         res = drive.get("{0}.yaml".format(game))
         return yaml.load(res.read(), Loader=yaml.FullLoader)
         
+@app.get("/sites.txt", response_class=PlainTextResponse)
+def get_txt(background_tasks: BackgroundTasks, game: Optional[str] = None):
+    background_tasks.add_task(statcounter)
+    if game is None:
+        res = drive.get("minecraft.yaml")
+        data = yaml.load(res.read(), Loader=yaml.FullLoader)
+        txt = ""
+        for item in data:
+            txt = txt + item["domain"] + "\n"
+        return txt
+    else:
+        res = drive.get("{0}.yaml".format(game))
+        data = yaml.load(res.read(), Loader=yaml.FullLoader)
+        txt = ""
+        for item in data:
+            txt = txt + item["domain"] + "\n"
+        return txt
 
 
 if __name__ == "__main__":
